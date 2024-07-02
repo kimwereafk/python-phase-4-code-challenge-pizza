5f1c5d16c65dd79face1a9548b5988165de0b5c5
kimwereafk <collinskimwere@gmail.com>
2024-07-02 23:10:01 +0300

    initial commit

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from models import db, Restaurant, RestaurantPizza, Pizza
 from flask_migrate import Migrate
-from flask import Flask, request, make_response
+from flask import Flask, request, jsonify, make_response
 from flask_restful import Api, Resource
 import os
 
@@ -24,6 +24,55 @@ api = Api(app)
 def index():
     return "<h1>Code challenge</h1>"
 
+
+class RestaurantsResource(Resource):
+    def get(self):
+        restaurants = Restaurant.query.all()
+        return [restaurant.to_dict() for restaurant in restaurants], 200
+
+
+class RestaurantResource(Resource):
+    def get(self, id):
+        restaurant = Restaurant.query.get(id)
+        if restaurant:
+            return restaurant.to_dict(), 200
+        return {"error": "Restaurant not found"}, 404
+
+    def delete(self, id):
+        restaurant = Restaurant.query.get(id)
+        if restaurant:
+            db.session.delete(restaurant)
+            db.session.commit()
+            return '', 204
+        return {"error": "Restaurant not found"}, 404
+
+
+class PizzasResource(Resource):
+    def get(self):
+        pizzas = Pizza.query.all()
+        return [pizza.to_dict() for pizza in pizzas], 200
+
+
+class RestaurantPizzasResource(Resource):
+    def post(self):
+        data = request.get_json()
+        try:
+            restaurant_pizza = RestaurantPizza(
+                price=data['price'],
+                pizza_id=data['pizza_id'],
+                restaurant_id=data['restaurant_id']
+            )
+            db.session.add(restaurant_pizza)
+            db.session.commit()
+            return restaurant_pizza.to_dict(), 201
+        except ValueError as e:
+            return {"errors": [str(e)]}, 400
+
+
+api.add_resource(RestaurantsResource, '/restaurants')
+api.add_resource(RestaurantResource, '/restaurants/<int:id>')
+api.add_resource(PizzasResource, '/pizzas')
+api.add_resource(RestaurantPizzasResource, '/restaurant_pizzas')
 
 if __name__ == "__main__":
     app.run(port=5555, debug=True)
