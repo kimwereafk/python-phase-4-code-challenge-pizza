3ae00dadcb3a6353d7250029193c2f10ff330943
kimwereafk <collinskimwere@gmail.com>
2024-07-04 12:00:50 +0300

    update

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from models import db, Restaurant, RestaurantPizza, Pizza
 from flask_migrate import Migrate
-from flask import Flask, request, jsonify, make_response
+from flask import Flask, request, jsonify
 from flask_restful import Api, Resource
 import os
 
@@ -13,23 +13,18 @@ app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
-migrate = Migrate(app, db)
-
 db.init_app(app)
-
+migrate = Migrate(app, db)
 api = Api(app)
-
 
 @app.route("/")
 def index():
     return "<h1>Code challenge</h1>"
 
-
 class RestaurantsResource(Resource):
     def get(self):
         restaurants = Restaurant.query.all()
         return [restaurant.to_dict() for restaurant in restaurants], 200
-
 
 class RestaurantResource(Resource):
     def get(self, id):
@@ -46,12 +41,10 @@ class RestaurantResource(Resource):
             return '', 204
         return {"error": "Restaurant not found"}, 404
 
-
 class PizzasResource(Resource):
     def get(self):
         pizzas = Pizza.query.all()
         return [pizza.to_dict() for pizza in pizzas], 200
-
 
 class RestaurantPizzasResource(Resource):
     def post(self):
@@ -66,9 +59,7 @@ class RestaurantPizzasResource(Resource):
             db.session.commit()
             return restaurant_pizza.to_dict(), 201
         except ValueError as e:
-            return {"errors": ["validation errors"]}, 400  # Adjust error message
-
-
+            return {"errors": ["validation errors"]}, 400
 
 api.add_resource(RestaurantsResource, '/restaurants')
 api.add_resource(RestaurantResource, '/restaurants/<int:id>')
